6f2987a4471e30bd06c2d001c52031f6b2716ac9
Christian <chrbudtz@gmail.com>
2021-04-22 09:22:11 +0200

    Init Commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:14-alpine
+
+WORKDIR /buildpath
+
+COPY ./package.json ./
+COPY ./yarn.lock ./
+
+RUN yarn install
+
+COPY ./ .
+#default url
+ARG NEXT_PUBLIC_API_URL=localhost:1377
+RUN NEXT_PUBLIC_API_URL=${NEXT_PUBLIC_API_URL}/ && yarn build
+
+EXPOSE 3000
+
+CMD ["yarn", "start"]
--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <p>
+          APIURL: {process.env.NEXT_PUBLIC_API_URL}
+        </p>
 
         <p className={styles.description}>
           Get started by editing{' '}
