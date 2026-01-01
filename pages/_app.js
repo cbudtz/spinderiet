3a3d6215200439467b3ec2ab07e7a3191f02ea75
Christian <chrbudtz@gmail.com>
2026-01-01 15:31:21 +0100

    tracking page views

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="http://umami.4a4b.dk/script.js"
+        data-website-id="c6b3e44d-362b-42a6-829a-02a381e845b3"
+        strategy="afterInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
